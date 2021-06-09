91437a51ecc6c0923629bfd550e040cd9c0042b7
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-09 15:17:12 -0700

    Initial file add

---
+++ app.js
@@ -0,0 +1,73 @@
+// *************** for Red
+
+
+function hideElement() {
+    document.getElementById("hide").style.visibility = "hidden";  
+}
+
+function showElement() {
+    document.getElementById("hide").style.visibility = "visible";  
+}
+
+
+// *************** For red
+
+var vid = document.getElementById("myVideo");
+
+function enableAutoplay() { 
+  vid.autoplay = true;
+  vid.load();
+}
+
+
+
+// // *************** for ylw
+
+
+// function hideElement() {
+//     document.getElementById("hide2").style.visibility = "hidden";  
+// }
+
+// function showElement() {
+//     document.getElementById("hide2").style.visibility = "visible";  
+// }
+
+
+// // *************** For ylw
+
+// var vid = document.getElementById("myVideo222");
+
+// function enableAutoplay2() { 
+//   vid.autoplay = true;
+//   vid.load();
+// }
+
+
+
+// // *************** for grrn
+
+
+// function hideElement() {
+//     document.getElementById("hide3").style.visibility = "hidden";  
+// }
+
+// function showElement() {
+//     document.getElementById("hide3").style.visibility = "visible";  
+// }
+
+
+// // *************** For grn
+
+// var vid = document.getElementById("myVideo3");
+
+// function enableAutoplay3() { 
+//   vid.autoplay = true;
+//   vid.load();
+// }
+
+
+
+
+
+
+
